7cf1138cbf8f976f18299a816fdc6c482a5efe4d
amb <mikaelh1651@gmail.com>
2023-02-20 18:08:51 +0900

    feat(home_screen): appbar 적용 및 최초 빌드

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:image_editor/component/main_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteItem: onDeleteItem,
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  void onPickImage() {}
+  void onSaveImage() {}
+  void onDeleteItem() {}
 }
